1ad10bc641387e2af64ebf57f80befb76fb40046
Wiktor Wielgusiak <wiktor.wielgusiak16@gmail.com>
2022-12-13 01:30:00 +0100

    Adding a json file with all Polish rivers.
    Adding the  extensions needed to handle the file with the geojson extension and the mapbox-gl map.
    
    Adding a function that allows you to download data from a geojson file.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,7 +2,12 @@
 
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Geometry, FeatureCollection, GeoJsonProperties } from "geojson";
+import {
+  Geometry,
+  FeatureCollection,
+  GeoJsonProperties,
+  Feature,
+} from "geojson";
 
 const Map = () => {
   const [data, setData] = useState<
@@ -11,13 +16,31 @@ const Map = () => {
     type: "FeatureCollection",
     features: [],
   });
+  const [selectedRivers, setSelectedRivers] = useState<Feature[]>([]);
+
+  const filteringData = (props: string) => {
+    return data.features.filter((el) =>
+      el.properties?.RWB_NAME.includes(props)
+    );
+  };
+  const setRivers = (props: string) => {
+    if (!selectedRivers.some((el) => el.properties?.RWB_NAME.includes(props))) {
+      // If not, add it to the array
+      setSelectedRivers([...selectedRivers, ...filteringData(props)]);
+    }
+  };
   useEffect(() => {
     axios
-      .get("newPL.geojson")
+      .get("polishRivers.geojson")
       .then((res) => setData(JSON.parse(JSON.stringify(res.data))))
       .catch((err) => console.log(err));
   }, []);
-  return <div></div>;
+  return (
+    <div>
+      <button onClick={() => setRivers("Wisła")}>Wisła</button>
+      <button onClick={() => setRivers("Odra")}>Odra</button>
+    </div>
+  );
 };
 
 export default Map;
