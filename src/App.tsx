7d11d28958a362273bbca566cbc2aba8155f4784
WiktorWielgusiak <witia16@gmail.com>
2023-02-24 00:08:54 +0100

    chore: Clean up unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 /** @format */
 
-import mapboxgl from "mapbox-gl";
-import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import useLocalStorage from "./hooks/useLocalStorage";
-import { MapboxGLMap } from "./shared";
 import MapPage from "./pages/MapPage";
 function App() {
   const [firstOpen, setFirstOpen] = useLocalStorage("firstOpen", "true");
