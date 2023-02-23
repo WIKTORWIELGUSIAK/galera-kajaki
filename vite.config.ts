9a8c108ab2393b04cf97439489ebedf49130f02b
WiktorWielgusiak <witia16@gmail.com>
2023-02-23 19:10:00 +0100

    feat: Code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @format */
 
+import { defineConfig } from "vite";
+// @ts-ignore
+import { dependencies } from "./package.json";
+// import react from '@vitejs/plugin-react'
+
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-router-dom", "react-dom"],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
+});
