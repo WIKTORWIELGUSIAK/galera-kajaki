0bd5caa9e1523b1d8bf127e07dbfec542fba656f
Wiktor Wielgusiak <82612654+WIKTORWIELGUSIAK@users.noreply.github.com>
2023-02-24 02:08:12 +0100

    chore: change variable name

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 /** @format */
 
-export const CONFIG_URL = "https://galera-backend-service.vercel.app";
+export const SERVER_URL = "https://galera-backend-service.vercel.app";
 // export const SERVER_URL = "http://localhost:3001";
